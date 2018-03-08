27eec531edc2ce9c6bc08a1db3a6e9eba1ca94e6
Urban Lavbič <urban@reveel.it>
2018-03-08 11:45:15 +0100

    Commit

--- util/src/log/log.util.ts
+++ util/src/log/log.util.ts
@@ -5,64 +5,67 @@ import { Config } from '@libs/models';
 // Lodash
 import isNil from 'lodash/isNil';
 
-declare const window: { ENV: Config };
-
 export function logDebug(code: LogCode, message?: string) {
-  _log(LogSeverity.DEBUG, code, message);
+	_log(LogSeverity.DEBUG, code, message);
 }
 
 export function logInfo(code: LogCode, message?: string) {
-  _log(LogSeverity.INFO, code, message);
+	_log(LogSeverity.INFO, code, message);
 }
 
 export function logWarn(code: LogCode, message?: string) {
-  _log(LogSeverity.WARN, code, message);
+	_log(LogSeverity.WARN, code, message);
 }
 
 export function logError(code: LogCode, message?: string) {
-  _log(LogSeverity.ERROR, code, message);
+	_log(LogSeverity.ERROR, code, message);
 }
 
 function _log(severity: LogSeverity, code: LogCode, message?: string): void {
-  if (!isBrowser()) {
-    return;
-  }
-  switch (severity) {
-    case LogSeverity.DEBUG: {
-      if (!window.ENV.env.debug) {
-        return;
-      }
-      // tslint:disable-next-line:no-console
-      console.debug(_sanitizeMessage(code, message));
-      break;
-    }
-    case LogSeverity.INFO: {
-      if (window.ENV.env.production) {
-      } else {
-        // tslint:disable-next-line:no-console
-        console.info(_sanitizeMessage(code, message));
-      }
-      break;
-    }
-    case LogSeverity.WARN: {
-      if (window.ENV.env.production) {
-      } else {
-        // tslint:disable-next-line:no-console
-        console.warn(_sanitizeMessage(code, message));
-      }
-      break;
-    }
-    case LogSeverity.ERROR: {
-      if (window.ENV.env.production) {
-      } else {
-        // tslint:disable-next-line:no-console
-        console.info(_sanitizeMessage(code, message));
-      }
-      break;
-    }
-  }
+	if (!isBrowser()) {
+		return;
+	}
+	switch (severity) {
+		case LogSeverity.DEBUG: {
+			if (!window.config.env.debug) {
+				return;
+			}
+			// tslint:disable-next-line:no-console
+			console.debug(_sanitizeMessage(code, message));
+			break;
+		}
+		case LogSeverity.INFO: {
+			if (window.config.env.production) {
+			} else {
+				// tslint:disable-next-line:no-console
+				console.info(_sanitizeMessage(code, message));
+			}
+			break;
+		}
+		case LogSeverity.WARN: {
+			if (window.config.env.production) {
+			} else {
+				// tslint:disable-next-line:no-console
+				console.warn(_sanitizeMessage(code, message));
+			}
+			break;
+		}
+		case LogSeverity.ERROR: {
+			if (window.config.env.production) {
+			} else {
+				// tslint:disable-next-line:no-console
+				console.info(_sanitizeMessage(code, message));
+			}
+			break;
+		}
+	}
 }
 
 function _sanitizeMessage(code: LogCode, message?: string): string {
-  return new Date().toISOString() + ':' + code + (!isNil(message) ? ':' + message : '');
+	return (
+		new Date().toISOString() +
+		':' +
+		code +
+		(!isNil(message) ? ':' + message : '')
+	);
 }
