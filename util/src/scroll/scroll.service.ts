2b318f4b26458bf984f9a057fe4e27f5bbe067f0
Urban Lavbič <urban@reveel.it>
2018-03-29 11:50:10 +0200

    Minor fix for Scroll Service

--- util/src/scroll/scroll.service.ts
+++ util/src/scroll/scroll.service.ts
@@ -8,47 +8,47 @@ import { PlatformService } from '../platform/index';
 
 @Injectable()
 export class ScrollService {
-	constructor(private _platform: PlatformService, private _router: Router) {}
+  constructor(private _platform: PlatformService, private _router: Router) {}
 
-	scrollToTop() {
-		this._scroll();
-	}
+  scrollToTop() {
+    this._scroll();
+  }
 
-	async navigateAndScroll(
-		scroll: Scroll,
-		route: string[],
-		routeExtras: NavigationExtras
-	) {
-		try {
-			const res = await this._router.navigate(route, routeExtras);
-			if (res) {
-				this._scroll(scroll);
-			}
-		} catch {}
-	}
+  async navigateAndScroll(scroll: Scroll, route: string[], routeExtras: NavigationExtras) {
+    try {
+      const res = await this._router.navigate(route, routeExtras);
+      if (res) {
+        this._scroll(scroll);
+      }
+    } catch {}
+  }
 
-	async navigateAndScrollToTop(route: string[], routeExtras: NavigationExtras) {
-		this.navigateAndScroll(
-			{
-				top: 0,
-				left: 0,
-				behavior: ScrollBehavior.SMOOTH
-			},
-			route,
-			routeExtras
-		);
-	}
+  async navigateAndScrollToTop(route: string[], routeExtras: NavigationExtras) {
+    this.navigateAndScroll(
+      {
+        top: 0,
+        left: 0,
+        behavior: ScrollBehavior.SMOOTH
+      },
+      route,
+      routeExtras
+    );
+  }
 
-	private _scroll(
-		data: Scroll = {
-			top: 0,
-			left: 0,
-			behavior: ScrollBehavior.SMOOTH
-		}
-	) {
-		if (!this._platform.isBrowser()) {
-			return;
-		}
-		window.scroll(data);
-	}
+  private _scroll(
+    data: Scroll = {
+      top: 0,
+      left: 0,
+      behavior: ScrollBehavior.SMOOTH
+    }
+  ) {
+    if (!this._platform.isBrowser()) {
+      return;
+    }
+    window.scroll({
+      top: data.top || 0,
+      left: data.left || 0,
+      behavior: data.behavior || ScrollBehavior.SMOOTH
+    });
+  }
 }
